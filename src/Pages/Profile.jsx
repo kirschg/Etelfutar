c17f17648b6c4286592d3bede7c62544d961a1db
kirschg <kirschg@gmail.com>
2025-03-18 09:22:24 +0100

    Completed temporary appearance of Profile. Filled with temporary data. Awaiting backend fixes.

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import '../Style.css';
@@ -10,11 +10,11 @@ export const YourProfile = () => {
   const [user, setUser] = useState([]);
   useEffect(() => {
     if (token !== null) {
-      axios.get("https://localhost:7106/Felhasznalok/GetFelhasznaloByTokenAsync?token=" + token, {
+      /*axios.get("https://localhost:7106/Felhasznalok/GetFelhasznaloByTokenAsync?token=" + token, {
         headers: { "Authorization": `Bearer ${token}` }
       })
         .then(res => setUser(res.data))
-        .catch(err => console.log(err))
+        .catch(err => console.log(err))*/
     }
     else {
       navigate("/Login");
@@ -23,11 +23,32 @@ export const YourProfile = () => {
   return (console.log(user),
     <div className="App">
       <div className="Panel">
-        <h1 style={{textAlign:"left"}}>{user.felhasznaloNev}</h1>
+        {/*<h1 style={{ textAlign: "left" }}>{user.felhasznaloNev}</h1>
         <div>
-            
+          <ul style={{listStyle:"none"}}>
+            <li>
+              <h4>{user.teljesNev}</h4>
+              <p style={{ color: "darkgrey" }}>{user.varos.nev + ", " + user.lakcim}</p>
+            </li>
+            <li>
+              <p>{user.email}</p>
+            </li>
+          </ul>
+        </div>*/}
+        <h1 style={{ textAlign: "left" }}>Teszt felhasználó név</h1>
+        <div>
+          <ul style={{listStyle:"none"}}>
+            <li>
+              <h4>Teszt teljes név</h4>
+              <p style={{ color: "darkgrey" }}>Teszt város + lakcím adat</p>
+            </li>
+            <li>
+              <p>Teszt email</p>
+            </li>
+          </ul>
         </div>
+        <Link className="button">Edit</Link>
       </div>
     </div>
-    )
+  )
 }
