e4234f6db285ba49a429e973c607f9df29d7cf90
kirschg <kirschg@gmail.com>
2025-03-24 13:34:51 +0100

    Small Changes

---
+++ EtelfutarAPI/DTOs/EtelekLearazasDTO.cs
@@ -0,0 +1,20 @@
+﻿using EtelfutarAPI.Models;
+
+namespace EtelfutarAPI.DTOs
+{
+    public class EtelekLearazasDTO
+    {
+        public EtelekLearazasDTO(Learaza data)
+        {
+            EtteremId = data.EtteremId;
+            EtelId = data.EtelId;
+            Learazas = data.Learazas;
+        }
+        public int EtteremId { get; set; }
+
+        public int EtelId { get; set; }
+
+        public int Learazas { get; set; }
+
+    }
+}
--- EtelfutarAPI/obj/Debug/net7.0/EtelfutarAPI.AssemblyInfo.cs
+++ EtelfutarAPI/obj/Debug/net7.0/EtelfutarAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EtelfutarAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c9ad358f6b536f88fac636f02623319efcc61ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("EtelfutarAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EtelfutarAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
