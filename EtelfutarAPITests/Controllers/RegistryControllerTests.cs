d452b1fbb6dcf0159d60d3ce6f2696613364db90
akoshub <149141822+akoshub@users.noreply.github.com>
2025-04-07 20:15:13 +0200

    Minden xunit test kész.

---
+++ EtelfutarAPITests/Controllers/RegistryControllerTests.cs
@@ -0,0 +1,74 @@
+﻿using EtelfutarAPI.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+using static System.Net.WebRequestMethods;
+
+namespace EtelfutarAPI.Controllers.Tests
+{
+    public class RegistryControllerTests
+    {
+        [Fact()]
+        public void PostregistryTest()
+        {
+            HttpClient client = new HttpClient()
+            {
+                BaseAddress = new Uri("http://localhost:5000")
+            };
+
+            string url = $"/api/Registry";
+
+            Felhasznalok ujRegisztracio = new Felhasznalok
+            {
+                Email = "timkoa@kkszki.hu",
+                FelhasznaloNev = "timike",
+                TeljesNev = "Timkó Ákos",
+                Hash = "2592d23d53bc62c8f324ccfee9b1092078bbf48b6826a64871408ebfd1af667a",
+                VarosId = 2,
+                Lakcim = "Otthon Utca 49",
+                Salt = "VLTIAPZvfcFzkY6fc0Vc8OGk0WMjmRvYZ0QVozRHq7IBotj8hMn4w5GqqyPh"
+            };
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                PropertyNameCaseInsensitive = true,
+            };
+
+            string requestjson = JsonSerializer.Serialize(ujRegisztracio, JsonSerializerOptions.Default);
+            var requestbody = new StringContent(requestjson, Encoding.UTF8, "application/json");
+            var requestResult = client.PostAsync(url, requestbody);
+
+            Xunit.Assert.Equal("OK", requestResult.Status.ToString());
+        }
+        
+        [Fact()]
+        public void GetregistryTest()
+        {
+            HttpClient client = new HttpClient()
+            {
+                BaseAddress = new Uri("http://localhost:5000")
+            };
+
+            string felhasznaloNev = "timike";
+            string email = "timkoa@kkszki.hu";
+
+            string url = $"api/Registry?felhasznaloNev={felhasznaloNev}&email={email}";
+            
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                PropertyNameCaseInsensitive = true,
+            };
+
+            string requestjson = JsonSerializer.Serialize(JsonSerializerOptions.Default);
+            var requestbody = new StringContent(requestjson, Encoding.UTF8, "application/json");
+            var requestResult = client.PostAsync(url, requestbody);
+
+            Xunit.Assert.Equal("OK", requestResult.Status.ToString());
+        }
+    }
+}
---
+++ EtelfutarAPITests/Controllers/RendeltetelControllerTests.cs
@@ -0,0 +1,128 @@
+﻿using Xunit;
+using EtelfutarAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using VizsgaremekAPI.Controllers;
+using VizsgaremekAPI.DTOs;
+using System.Security.Cryptography;
+
+namespace EtelfutarAPI.Controllers.Tests
+{
+    public class RendeltetelControllerTests
+    {
+        [Fact()]
+        public async void GetRendeletelAsyncTest()
+        {
+            HttpClient client = new HttpClient()
+            {
+                BaseAddress = new Uri("http://localhost:5000")
+            };
+            string url = "/Rendeltetel/GetRendeltetelAsync";
+
+            var result = await client.GetAsync(url);
+
+            Xunit.Assert.Equal("OK", result.StatusCode.ToString());
+        }
+
+        public static string CreateSHA256(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] data = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                return sBuilder.ToString();
+            }
+        }
+
+        [Fact()]
+        public async void PostRendeletelAsyncTest()
+        {
+            HttpClient client = new HttpClient()
+            {
+                BaseAddress = new Uri("http://localhost:5000")
+            };
+            int etelId = 10;
+            int felhasznaloId = 9;
+
+            string url = $"/Rendeltetel/PostRendeltetelAsync?etelId={etelId}&felhasznaloId={felhasznaloId}";
+
+            var result = await client.PostAsync($"api/Login/GetSalt/timike", new StringContent("asdfgh", Encoding.UTF8, "text/plain"));
+            string salt = await result.Content.ReadAsStringAsync();
+            string tmpHash = CreateSHA256("asdfgh" + salt);
+            LoginDTO loginDTO = new LoginDTO()
+            {
+                LoginName = "timike",
+                TmpHash = tmpHash,
+            };
+            string json = JsonSerializer.Serialize(loginDTO, JsonSerializerOptions.Default);
+            var body = new StringContent(json, Encoding.UTF8, "application/json");
+            var postResult = await client.PostAsync("api/Login", body);
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                PropertyNameCaseInsensitive = true,
+            };
+            string valaszJson = await postResult.Content.ReadAsStringAsync();
+            LoggedUser loggedUser = JsonSerializer.Deserialize<LoggedUser>(valaszJson, options);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", loggedUser.Token);
+            string requestjson = JsonSerializer.Serialize(JsonSerializerOptions.Default);
+            var requestbody = new StringContent(requestjson, Encoding.UTF8, "application/json");
+            var requestResult = await client.PostAsync(url, requestbody);
+
+            Xunit.Assert.Equal("OK", requestResult.StatusCode.ToString());
+        }
+
+        [Fact()]
+        public async void DeleteExcludedetelAsyncTest()
+        {
+            HttpClient client = new HttpClient()
+            {
+                BaseAddress = new Uri("http://localhost:5000")
+            };
+            int etelId = 9;
+            int felhasznaloId = 8;
+
+            string url = $"/Rendeltetel/DeleteRendeltetelAsync?etelId={etelId}&felhasznaloId={felhasznaloId}";
+
+
+            var result = await client.PostAsync($"api/Login/GetSalt/timike", new StringContent("asdfgh", Encoding.UTF8, "text/plain"));
+            string salt = await result.Content.ReadAsStringAsync();
+            string tmpHash = CreateSHA256("asdfgh" + salt);
+            LoginDTO loginDTO = new LoginDTO()
+            {
+                LoginName = "timike",
+                TmpHash = tmpHash,
+            };
+            string json = JsonSerializer.Serialize(loginDTO, JsonSerializerOptions.Default);
+            var body = new StringContent(json, Encoding.UTF8, "application/json");
+            var postResult = await client.PostAsync("api/Login", body);
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                PropertyNameCaseInsensitive = true,
+            };
+            string valaszJson = await postResult.Content.ReadAsStringAsync();
+            LoggedUser loggedUser = JsonSerializer.Deserialize<LoggedUser>(valaszJson, options);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", loggedUser.Token);
+            string requestjson = JsonSerializer.Serialize(JsonSerializerOptions.Default);
+            var requestbody = new StringContent(requestjson, Encoding.UTF8, "application/json");
+            var requestResult = await client.DeleteAsync(url);
+
+            Xunit.Assert.Equal("OK", requestResult.StatusCode.ToString());
+        }
+    }
+}
